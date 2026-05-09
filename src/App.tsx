2be9bce899f478cfc0a45e6a72b9d73d061de801
Giulia Creps <giulia.creps2@gmail.com>
2026-05-09 11:01:39 +0200

    Fix BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/gc-backend-portofolio">
       <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
